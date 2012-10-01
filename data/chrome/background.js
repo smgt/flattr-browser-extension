4b8468d2aca67bada7a7aab5b2d8d08761cdbbe2
Simon Gate <simon@smgt.me>
2012-10-01 10:17:50 +0200

    Keep settings for each tab

--- data/chrome/background.js
+++ data/chrome/background.js
@@ -1,7 +1,4 @@
-var lookupUrl = undefined;
-var relPaymentLink = undefined;
-var canonicalUrl = undefined;
-var flattrable = undefined;
+var tabs = {};
 
 // Enable all the options in localstorage
 if(!localStorage.getItem("flattr.options")) {
@@ -26,18 +23,24 @@ if(!localStorage.getItem("flattr.first_run")) {
 // exists, we show our extension icon in the address field.
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if(changeInfo.status === "complete" && tab && tab.url && tab.url.match(/^https?:\/\//)) {
-    lookupUrl = undefined;
+
+    var thisTab = {};
+    tabs[tabId] = thisTab;
 
     if (Wikipedia.validURL(tab.url)) {
-      flattrable = true;
-      lookupUrl = Wikipedia.autosubmitURL(tab.url, tab.title);
+      thisTab.flattrable = true;
+      thisTab.lookupUrl = Wikipedia.autosubmitURL(tab.url, tab.title);
       pageActionShow(tabId);
     } else {
       showFlattrButtonIfThingExistsForUrl(tab.url, tabId, function(url) {
-        lookupUrl = url;
+        thisTab.lookupUrl = url;
       });
     }
   }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  delete tabs[tabId];
 });
 
 function pageActionShow(tabId) {
@@ -50,32 +53,35 @@ function pageActionShow(tabId) {
 
 function showFlattrButtonIfThingExistsForUrl(urlToTest, tabId, callback) {
 
-    if(!urlToTest.match(/^https?:\/\//)) return;
-    if(urlToTest.match(/^https?:\/\/(?:\w+\.)?flattr.com\/?/)) return;
+  if(!urlToTest.match(/^https?:\/\//)) return;
+  if(urlToTest.match(/^https?:\/\/(?:\w+\.)?flattr.com\/?/)) return;
 
-    Flattr.lookupURL(urlToTest, function(response) {
-        var url;
+  var thisTab = tabs[tabId];
 
-        if (response.message === 'flattrable') {
-            url = Flattr.autosubmitURL({url:urlToTest});
-        } else if (response && response.link) {
-            url = response.link;
-        }
+  Flattr.lookupURL(urlToTest, function(response) {
+      var url;
 
-        if (url) {
-            flattrable = true;
-            pageActionShow(tabId);
-            callback(url);
-        }
-    });
+      if (response.message === 'flattrable') {
+          url = Flattr.autosubmitURL({url:urlToTest});
+      } else if (response && response.link) {
+          url = response.link;
+      }
+
+      if (url) {
+          thisTab.flattrable = true;
+          pageActionShow(tabId);
+          callback(url);
+      }
+  });
 }
 
 // When the icon in address field is clicked, we open the flattr.com
 // thing page in a new tab/window.
 chrome.pageAction.onClicked.addListener(function (tab) {
-    if(flattrable) {
+    var thisTab = tabs[tab.id];
+    if(thisTab.flattrable) {
       chrome.tabs.create({
-        url: (lookupUrl || relPaymentLink || canonicalUrl)
+        url: (thisTab.lookupUrl || thisTab.relPaymentLink || thisTab.canonicalUrl)
       });
     } else {
       return;
@@ -91,20 +97,24 @@ chrome.extension.onConnect.addListener(function(port) {
 
     // Wait for a URL from a contentscript
     port.onMessage.addListener(function(msg, port) {
+      if(port.sender === undefined) return;
+
+      var thisTab = tabs[port.sender.tab.id];
+
       if( msg.url ) {
         chrome.tabs.create({
           url:Flattr.autosubmitURL({url:msg.url})
         });
       } else if (msg.relPaymentLink) {
         if(port.sender.tab) {
-          relPaymentLink = msg.relPaymentLink;
-          flattrable = true;
+          thisTab.relPaymentLink = msg.relPaymentLink;
+          thisTab.flattrable = true;
           pageActionShow(port.sender.tab.id);
         }
       } else if (msg.canonicalUrl) {
         if(port.sender.tab) {
           showFlattrButtonIfThingExistsForUrl(msg.canonicalUrl, port.sender.tab.id, function(url) {
-              canonicalUrl = url;
+              thisTab.canonicalUrl = url;
           });
         }
       }
